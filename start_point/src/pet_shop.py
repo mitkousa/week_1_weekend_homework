211523f89d081988d758232f78d923e938818e1e
mitkousa <uk.dimitar@gmail.com>
2021-07-26 16:15:39 +0100

    final commit

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -18,16 +18,11 @@ def add_or_remove_cash(total_cash, cash):
 def get_pets_sold(sold_pets):
     return sold_pets["admin"]["pets_sold"]
 
-def increase_pets_sold(total_sold, increased_pets,):
-        increased_pets = 2
+def increase_pets_sold(total_sold, increased_pets):
         total_sold["admin"]["pets_sold"] += increased_pets
-        return total_sold
 
-def get_stock_count(pets):
-    count = []
-    for pet in pets["pets"]:
-        count.append(pet["pet_type"])
-    return len(count)
+def get_stock_count(shop):
+    return len(shop["pets"])
 
 def get_pets_by_breed(pets, pet_breed):
     found = []
